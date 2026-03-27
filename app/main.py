f7b77870081d0e14c32ee9521eb047199b34574d
Ullbergm <ullbergm2@arcada.fi>
2026-03-27 11:37:28 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return { "msg": "Hello local docker!" }
+
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello Max!" 
+}
