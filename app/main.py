f499af173fc2ba8563eb81dada426c179db000d5
Ullbergm <ullbergm2@arcada.fi>
2026-03-27 12:02:36 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello local docker!" }
+    return {"msg": "Hello local docker!"}
 
 @app.get("/hello")
 def hello():
-    return { "msg": "Hello Max!" 
-}
+    return {"msg": "Hello Max!"}
+
+@app.get("/api/ip")
+async def get_ip(request: Request):
+    client_ip = request.client.host
+    return {"ip": client_ip}
+
+@app.get("/ip", response_class=HTMLResponse)
+async def get_ip_html(request: Request):
+    client_ip = request.client.host
+    return f"<h1>Din publika IP-adress är {client_ip}</h1>"
